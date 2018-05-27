814a71e781fb6d38505e51526ebaee469276e38e
zhangchengzheng <zhangchengzheng@wecash.net>
2018-05-27 16:13:51 +0800

    edit eslint

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join([ 'Hello', 'webpack', 'lueluelue', 123, 456 ], ' ')
-  console.log('test pre-commit')
-  abc = 3
-  var1 = 3
-  var2 = 4
-  alert(3333333)
-  alert(4444444)
   return element
 }
 
