644c1a864072e96900db07adbe5a73879eed3139
zhangchengzheng <zhangchengzheng@wecash.net>
2018-06-04 15:57:18 +0800

    指南：管理资源

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import _ from 'lodash'
+//import _ from 'lodash'
+import './style.css'
+import './testSass.scss'
+import './testLess.less'
+//import photo from './asset/photo.jpg'
+import printMe from './print'
 
 function component() {
   var element = document.createElement('div')
+  const btn = document.createElement('button')
+
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join([ 'Hello', 'webpack', 'lueluelue', 123, 456 ], ' ')
+  //element.innerHTML = _.join([ 'Hello', 'webpack' ], ' ')
+  //element.classList.add('hello')
+  //element.classList.add('test-sass')
+  //element.classList.add('test-less')
+  //const img = new Image()
+  //img.src = photo
+  //element.appendChild(img)
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,53 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 // TODO: asset managment
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle01.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 }
