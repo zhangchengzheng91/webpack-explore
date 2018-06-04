429db1ea24f78ad2960637d614e735222f4dd513
zhangchengzheng <zhangchengzheng@wecash.net>
2018-06-04 17:13:18 +0800

    指南：管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 
 // TODO: asset managment
@@ -13,6 +14,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins:[
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
