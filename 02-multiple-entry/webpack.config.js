5c835291e30e4ec715fd017eebbdc38777e4ec48
zhangchengzheng <zhangchengzheng91@163.com>
2020-08-08 20:55:56 +0800

    new feature:multiple entry webapck

---
+++ 02-multiple-entry/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '/dist')
+  },
+  mode: 'production'
+}
