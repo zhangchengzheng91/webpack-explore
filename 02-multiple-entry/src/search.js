6ce1d1acb215ecbaf060226af32bd571abfc5c24
zhangchengzheng <zhangchengzheng91@163.com>
2020-08-11 17:48:39 +0800

    new feature: 增加 webapack 基本功能 demo

--- 02-multiple-entry/src/search.js
+++ 02-multiple-entry/src/search.js
@@ -1,3 +1,30 @@
-import hello from './hello'
+//import hello from './hello'
+//
+//document.write(hello('search'))
 
-document.write(hello('search'))
+import React from 'react'
+import ReactDom from 'react-dom'
+import './search.css'
+import './search.less'
+import exampleImage from '../assets/image/example-image.png'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className='search'>Search Text watch</div>
+        <div>
+          <img src={exampleImage} alt='exampleImage'/>
+        </div>
+        <div style={{
+          backgroundImage: 'url("../assets/image/example-image.png")'
+        }}>
+          this is inline style
+        </div>
+      </div>
+
+    )
+  }
+}
+
+ReactDom.render(<Search/>, document.getElementById('root'))
--- 02-multiple-entry/webpack.config.js
+++ 02-multiple-entry/webpack.config.js
@@ -7,7 +7,41 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, '/dist')
+    path: path.join(__dirname, '/dist'),
+    publicPath: './dist/'
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      }, {
+        test: /\.(png|jpg|gif)$/,
+        //use: 'file-loader',
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1024 * 100 // 如果图片体积小于 100 KB，自动将图片转换为 base64
+          }
+        }],
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader',
+        //use: 'url-loader',
+      }
+    ]
+  },
+  watch: true, // 默认 false，也就是不开启
+  watchOptions: { // 只有开启监听模式时，watchOptions 才有意义
+    ignored: /node_modules/, // 默认为空，不监听的文件或者文件夹，支持正则匹配
+    aggregateTimeout: 300, // 监听到变化后会等 300ms 再去执行，默认 300ms
+    poll: 1000, // 判断文件是否发生变化时通过不停询问系统指定文件有没有变化实现的，1000ms 询问一次
+  }
 }
