3bfb9672f9038293424ddf17eafd1b059f0e7e04
zhangchengzheng <zhangchengzheng@wecash.net>
2020-08-16 16:07:05 +0800

    new feature: add 移动端CSS-px自动转换成rem

--- 03-webpack进阶用法/webpack.prod.js
+++ 03-webpack进阶用法/webpack.prod.js
@@ -42,6 +42,13 @@ module.exports = {
                 })
               ]
             }
+          },
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              remPrecision: 8,
+            }
           }
         ]
       }, {
